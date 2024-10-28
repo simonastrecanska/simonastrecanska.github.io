6b431febeb45369026216046a20367d339029a0d
Simona Strečanská <simona.strec@gmail.com>
2024-10-28 19:27:21 +0100

    first commit

---
+++ src/data/constants.ts
@@ -0,0 +1,26 @@
+export const author = "Lance Ross";
+
+export const defaultMeta = {
+  title: "AstroZinc",
+  description:
+    "An Astro blogging template made by @exylons using Astro, Tailwind, and MDX",
+  ogImage: {
+    src: "/static/banner.png",
+    alt: "Astro Zinc with Astro Logo",
+  },
+};
+
+export const navigation = [
+  {
+    href: "/",
+    text: "Home",
+  },
+  {
+    href: "/blog/",
+    text: "Blog",
+  },
+  {
+    href: "/rss.xml",
+    text: "RSS",
+  },
+];
