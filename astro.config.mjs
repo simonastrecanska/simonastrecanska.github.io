4d7afe072e2be67f4c0852e3086de68da0575ac5
Simona Strečanská <simona.strec@gmail.com>
2024-10-28 20:02:06 +0100

    fix deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -53,7 +53,7 @@ export default defineConfig({
   image: {
     service: sharpImageService(),
   },
-  site: "https://github.com/simonastrecanska/simonastrecanska.github.io.git",
+  site: "https://simonastrecanska.github.io.git",
   vite: {
     plugins: [rawFonts([".ttf", ".woff"])],
     optimizeDeps: {
