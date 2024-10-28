ff3f5312ccf443b2a2a5329c1eaf5afe4ac8ca89
Simona Strečanská <simona.strec@gmail.com>
2024-10-28 20:21:47 +0100

    delating cheerios

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig, sharpImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import { readFileSync } from "node:fs";
+import icon from "astro-icon";
 import expressiveCode from "astro-expressive-code";
 import mdx from "@astrojs/mdx";
 import rehypeSlug from "rehype-slug";
@@ -45,6 +46,7 @@ export default defineConfig({
     tailwind(),
     sitemap(),
     expressiveCode(astroExpressiveCodeOptions),
+    icon(),
     mdx(),
     react(),
   ],
